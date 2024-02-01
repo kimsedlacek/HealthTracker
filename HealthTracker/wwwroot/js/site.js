fbb21a74a8957cad79d472cf8947c39ce7de39c6
Kim Sedlacek <sedlaceks@optonline.net>
2024-02-01 08:57:53 -0500

    updates inc pinned row and columns

--- HealthTracker/wwwroot/js/site.js
+++ HealthTracker/wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+{
+    "Logging": {
+        "LogLevel": {
+            "Default": "Information",
+                "Microsoft.AspNetCore": "Warning"
+        }
+    },
+    "AllowedHosts": "*"
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the table element
+    const table = document.getElementById("data-table");
+
+    // Add a scroll event listener to handle scrolling
+    table.parentElement.addEventListener("scroll", function () {
+        // Get the left column cells
+        const leftColumnCells = table.querySelectorAll(".left-column");
+
+        // Iterate through each left column cell and update its left offset
+        leftColumnCells.forEach(function (cell) {
+            cell.style.left = `${table.parentElement.scrollLeft}px`;
+        });
+    });
+});
